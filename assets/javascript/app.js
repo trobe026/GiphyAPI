5d99a6819f8bf861172350baac87e2bfbe963771
Travis Roberts <travis.roberts2@gmail.com>
2017-12-05 23:09:54 -0600

    the final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,25 @@
 $( window ).on("load", function() {
 
-  var topics = ["dog", "giraffe", "panda"];
+  var topics = ["dog", "giraffe", "panda", "cat", "chipmunk", "elephant", "raccoon", "goat", "pig", "lion", "bear", "mouse", "bird", "ocelot", "dolphin"];
   // create buttons for each item in topics, assigning each item to an id for later
   function createButtons() {
     $.each(topics, function(index, value) {
+      var word = value.charAt(0).toUpperCase() + value.slice(1);
       var buttonItem = $("<button>");
-      buttonItem.attr("id", value);
-      buttonItem.append(value);
+      buttonItem.attr("id", word);
+      buttonItem.append(word);
       $('#buttonList').append(buttonItem);
     });
   }
   createButtons();
+
 // add an animal to the list with no dups
     $("#addNewAnimal").on("click", function() {
       event.preventDefault();
       var userInput = $("#newAnimal").val().trim();
       topics.push(userInput);
       $('#buttonList').html(" ");
+      $('#newAnimal').val('');
       createButtons();
     });
 // when button in buttonList is clicked the animal in the id of that button populates queryURL.  Ajax response is passed to results, for each item in results an img is created with data attributes for later, images are prepended to the #images div
@@ -46,6 +49,7 @@ $( window ).on("load", function() {
 // when an image on the page is clicked, the data attributes are assigned to variables, if the data-state is still, the img src is replaced by the url stored in the active variable from data-animate, data-state is also updated to active, so that when clicked again the function can run in reverse in the else block.
           })
           $('#images').on("click", 'img', function() {
+            console.log(this);
             var state = $(this).attr("data-state");
             var active = $(this).attr("data-animate");
             var still = $(this).attr("data-still");
